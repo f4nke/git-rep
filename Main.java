7fd159783634768ff7c2888164ba412738c969e6
f4nke <cool-vlad@mail.ru>
2019-10-19 21:03:58 +0300

    исправлено

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 // Первое задание
@@ -24,5 +23,34 @@ public class Main {
         System.out.println(Arrays.toString(arr));
 
         ArrayList<Object> newArr = toList(arr);
+// Четвертое задание
+        List<SomeClass> list = Arrays.asList(
+                new SomeClass(1, "Test1"),
+                new SomeClass(2, "Test1"),
+                new SomeClass(3, "Test1"),
+                new SomeClass(4, "Test2"),
+                new SomeClass(5, "Test2"),
+                new SomeClass(6, "Test3"),
+                new SomeClass(7, "Test3"),
+                new SomeClass(8, "Test4"));
+
+        HashMap<String, List<Integer>> map = new HashMap<>();
+
+        for (SomeClass item : list) {
+            if(!map.containsKey(item.name)) {
+                map.put(item.name, new ArrayList<>());
+            }
+            map.get(item.name).add(item.id);
+        }
+
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + Arrays.toString(entry.getValue().toArray()));
+        }
+
     }
+
+
+
+
+
 }
---
+++ SomeClass.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class SomeClass {
+    int id;
+    String name;
+
+    SomeClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
